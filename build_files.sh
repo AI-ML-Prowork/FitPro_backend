e7a0e693c0a8c26871046e9264c7c7e881f41bda
CFC-Tech-AI <pythoncfc@gmail.com>
2024-01-30 07:31:04 +0530

    second commit

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,6 @@ python3.9 manage.py migrate --noinput
 
 python3.9 manage.py createsuperuser --noinput \
     --username admin \
-    --password admin
+    --password admin@123
     
 python3.9 manage.py collectstatic --noinput --clear
